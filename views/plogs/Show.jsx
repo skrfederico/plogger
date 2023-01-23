692128daa38751167c9b4b20d94e3508b2703efc
federico <skrfederico@gmail.com>
2023-01-22 19:43:29 -0800

    functional needs getting prettier

--- views/plogs/Show.jsx
+++ views/plogs/Show.jsx
@@ -9,8 +9,8 @@ const Plog = require("../../models/plogs");
 
 class Show extends React.Component {
   render() {
-    const log = this.props;
-console.log(log._id)
+    const  plog  = this.props;
+// console.log(plog._id)
     // const query = Plog.find({});
 
     // Alternative syntax
@@ -18,27 +18,40 @@ console.log(log._id)
 
     return (
       <Layout>
-        <Heading> {log.title} Show Page </Heading>
-        <div className="shadow-[inset_-12px_-8px_40px_#46464620]">
-        <Text>
-          {log.body}
-          </Text>
-           <Commented>
-           {log.goodDay
-            ? "It was a good day"
-            : "It was not a good day..."} </Commented>
-            <Commented>
-            {"-"} {String(log.createdAt)}      
-            </Commented>
-       
-        <Button><form action={`/plogs/${log._id}?_method=DELETE`} method="POST">
-        <input type="submit" value="DELETE"/>
-  </form></Button>
-  
-                <Button><form action='/plogs' method="GET">
-                  <input type="submit" value="BACK" />
-                </form></Button>
-                </div>
+        <Heading> {plog.student} phonecall Show Page </Heading>
+
+        <div class="border-solid border-2 border-sky-500 grid grid-cols-4 gap-4">
+     <div>
+<h4 id="drawer-label" class="mb-1.5 leading-none text-xl font-semibold text-gray-900 dark:text-white">{plog.student}</h4>
+<p class="mb-5 text-xl font-bold text-gray-900 dark:text-white">{" on "}{plog.date}</p>
+</div>
+    <dl>
+        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Description</dt>
+        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{plog.description}</dd>
+    </dl>
+        <div>    <dl>
+        <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Called by:</dt>
+        <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{plog.teacher}</dd>
+    </dl>
+    </div>
+        <div><Button><form action={`/plogs/${plog._id}/edit`} method="GET">
+                <input type="submit" value="UPDATE"/>
+                </form></Button><Button><form
+                action={`/plogs/${plog._id}?_method=DELETE`}
+                method="POST">
+                <input type="submit" value="DELETE" />
+                </form></Button></div>
+              </div>
+
+
+
+              
+                
+        
+     
+     
+     
+     
       </Layout>
     );
   }
