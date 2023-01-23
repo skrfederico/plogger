994c4ab0dcee9a4453d781b5757949ac214775f4
federico <skrfederico@gmail.com>
2023-01-23 11:07:07 -0800

    routeController created

---
+++ controllers/routeController.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const router = express.Router()
+const Plog = require('../models/plogs.js')
+
+// add routes
+// Index
+router.get('/', (req, res) => {
+  // Use Plogs model to get all Plogs
+  Plog.find({}, (error, allPlogs) => {
+    res.render('plogs/Index', {
+      plogs: allPlogs
+    })
+  })
+})
+
+// New
+router.get('/new', (req, res) => {
+  res.render('plogs/New')
+})
+
+// Delete
+router.delete('/:id', (req, res) => {
+  // Delete document from collection
+  Plog.findByIdAndRemove(req.params.id, (err, plog) => {
+    res.redirect('/plogs')
+  })
+})
+
+// Update
+router.put('/:id', (req, res) => {
+  req.body.success = req.body.success === 'on' ? true : false
+  req.body.etiquette = req.body.etiquette === 'on' ? true : false
+
+  // Update the plog document using our model
+  Plog.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedModel) => {
+      res.redirect('/plogs')
+    }
+  )
+})
+
+// Create
+router.post('/', (req, res) => {
+  if (req.body.success === 'on') {
+    req.body.success = true
+  } else {
+    req.body.success = false
+  }
+  if (req.body.etiquette === 'on') {
+    req.body.etiquette = true
+  } else {
+    req.body.etiquette = false
+  }
+  // Use Model to create Plog Document
+  Plog.create(req.body, (error, createdPlog) => {
+    // Once created - respond to client
+    res.redirect('/plogs')
+  })
+})
+
+// Edit
+router.get('/:id/edit', (req, res) => {
+  // Find our document from the collection - using mongoose model
+  Plog.findById(req.params.id, (err, foundPlog) => {
+    // render the edit view and pass it the found plog
+    res.render('plogs/Edit', {
+      plog: foundPlog
+    })
+  })
+})
+
+// Show
+router.get('/:id', (req, res) => {
+  // Find the specific document
+  Plog.findById(req.params.id, (error, foundPlog) => {
+    // render the Show route and pass it the foundPlog
+    res.render('plogs/Show', {
+      plog: foundPlog
+    })
+  })
+})
+
+// export router
+module.exports = router
