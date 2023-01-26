c26918a90b7f2bc0540509f8183f0ec13caeb9c9
federico <skrfederico@gmail.com>
2023-01-26 11:13:04 -0800

    userController w async bcrypt

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+// const saltRounds = 10
 
-const user = require('../../models/user')
+// var salt =
+const User = require('../../models/user')
 
-router.post('/signup', (req, res) => {
-  res.send(req.body)
+router.post('/signup', async (req, res) => {
+  // hash the password that we receive
+  // we don't want to store passwords in plain text in our db
+  // hashing is different than encrypting (we are not doing this for now)
+  // hashes are used for comparisons
+  req.body.password = await bcrypt.hash(
+    req.body.password,
+    await bcrypt.genSalt(10)
+  )
+  // without async
+  // bcrypt.genSalt(10, function (err, salt) {
+  //   bcrypt.hash(process.env.SECRET, salt, function (err, hash) {
+  //     // Store hash in your password DB.
+  //     res.send(hash)
+  //   })
+  // })
 })
 
 router.post('/login', (req, res) => {
--- models/user.js
+++ models/user.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema(
   {
     //we can have a phone/password
     //the biggest data files are generally userSchemas
-    name: { type: String, required },
+    name: { type: String, required: true, unique: true },
     password: { type: String, required: true }
   },
   { timestamps: true }
 )
 
-const User = mongoose.model('User', plogSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
